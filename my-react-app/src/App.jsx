21d75e9ca29f01a7ba02ecc28bc69ed53e18a613
Simran Toor <simrankaurtoor@outlook.com>
2025-02-11 17:54:17 +0000

    navbar added

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,3 +1,4 @@
+import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
 
@@ -6,7 +7,10 @@ function App() {
     <>
       <Header />
       <main>
-        <h1>BLUEPRINT</h1>
+        <Container>
+          <h1>BLUEPRINT</h1>
+        </Container>
+
       </main>
       <Footer />
     </>
--- my-react-app/src/components/Header.jsx
+++ my-react-app/src/components/Header.jsx
@@ -1,5 +1,22 @@
+import { Container, Nav, Navbar } from 'react-bootstrap'
+
 const Header = () => {
-    return <header>header</header>
+    return(
+        <header>
+            <Navbar expand="lg" className="bg-body-tertiary" collapseOnSelect >
+                <Container>
+                    <Navbar.Brand href="/">BLUEPRINT</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ms-auto">
+                        <Nav.Link href="/cart">Cart</Nav.Link>
+                        <Nav.Link href="/login">Sign In</Nav.Link>
+                    </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>    
+    ) 
 }
 
 export default Header
