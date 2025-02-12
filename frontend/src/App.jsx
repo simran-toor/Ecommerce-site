ae22bbe811b5b175c6ab670e129bf7d00c353088
Simran Toor <simrankaurtoor@outlook.com>
2025-02-12 17:47:02 +0000

    react-router-dom and react-router-bootstrap added, Route added to App.jsx to route to homepage, <a> tags changed to Link tags, ProductPage created and routed to

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,24 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <HomePage />
+          <Routes>
+            <Route path="/" element={<HomePage />} exact />
+            <Route path="/product/:id" element={<ProductPage />} />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 }
 
