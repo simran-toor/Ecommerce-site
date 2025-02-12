b33e70e64430d5990dfbd477d5cd7ccaed9c1e8b
Simran Toor <simrankaurtoor@outlook.com>
2025-02-12 13:26:10 +0000

    all files formatted, icons added to cart and sign in, padding added to header

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,20 +1,19 @@
-import { Container } from 'react-bootstrap'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Header />
-      <main>
+      <main className="py-3">
         <Container>
           <h1>BLUEPRINT</h1>
         </Container>
-
       </main>
       <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- my-react-app/src/components/Footer.jsx
+++ my-react-app/src/components/Footer.jsx
@@ -1,17 +1,15 @@
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col } from "react-bootstrap";
 
 const Footer = () => {
-    return(
-        <footer>
-            <Container>
-                <Row>
-                    <Col className='text-center py-3'>
-                        Copyright &copy; BLUEPRINT
-                    </Col>
-                </Row>
-            </Container>
-        </footer>
-    ) 
-}
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col className="text-center py-3">Copyright &copy; BLUEPRINT</Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
