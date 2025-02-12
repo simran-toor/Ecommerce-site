2b1ff072953203b226badebf2608789de1abaf24
Simran Toor <simrankaurtoor@outlook.com>
2025-02-12 17:02:36 +0000

    rating icon color changed to yellow, propTypes added to Rating and Product components

--- my-react-app/src/components/Product.jsx
+++ my-react-app/src/components/Product.jsx
@@ -1,29 +1,38 @@
-import { Card } from "react-bootstrap"
+import { Card } from "react-bootstrap";
+import Rating from "./Rating";
+import PropTypes from "prop-types";
+
 
 const Product = ({ product }) => {
-    return (
-        <Card className="my-3 p-3 rounded">
-            <a href={`/product/${product._id}`}>
-                <Card.Img src={product.image} variant="top" />
-            </a>
+  return (
+    <Card className="my-3 p-3 rounded">
+      <a href={`/product/${product._id}`}>
+        <Card.Img src={product.image} variant="top" />
+      </a>
 
-            <Card.Body>
-                <a href={`/product/${product._id}`}>
-                    <Card.Title as="div">
-                        <strong>{product.name}</strong>
-                    </Card.Title>
-                </a>
+      <Card.Body>
+        <a href={`/product/${product._id}`}>
+          <Card.Title as="div">
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </a>
 
-                <Card.Text as="div">
-                    <div className="my-3">
-                        {product.rating} from {product.numReviews} reviews
-                    </div>
-                </Card.Text>
+        <Card.Text as="div">
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+            color="#f8e825"
+          />
+        </Card.Text>
 
-                <Card.Text as="h3">${product.price}</Card.Text>
-            </Card.Body>
-        </Card>
-    )
+        <Card.Text as="h3">${product.price}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
+Product.propTypes = {
+  product: PropTypes.object.isRequired,
 }
 
-export default Product
+export default Product;
