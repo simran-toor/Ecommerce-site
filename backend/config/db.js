1b99970e9202eb4372d0297f676536bbef35de13
Simran Toor <simrankaurtoor@outlook.com>
2025-02-16 17:37:19 +0000

    colors added for console text

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,10 +4,10 @@ const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI)
 
-        console.log(`MongoDB Connected: ${conn.connection.host}`)
-        
+        console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
+
     } catch (error) {
-        console.error(`Error: ${error.message}`)
+        console.error(`Error: ${error.message}`.brightRed.underline.bold)
         process.exit(1)
     }
 }
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import { config } from "dotenv";
 import products from "./data/products.js";
 import connectDB from "./config/db.js";
 import cors from "cors";
+import colors from "colors";
 
 config();
 
@@ -30,5 +31,5 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`.magenta.bold)
 );
